5611922d201264a362b21fa7a7fc10b4419d8b64
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-12-02 10:07:37 -0500

    finish my own solution of exercise 03 with refactor

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -7,6 +7,7 @@ import {Switch} from '../switch'
 // 🐨 create your ToggleContext context here
 // 📜 https://reactjs.org/docs/context.html#reactcreatecontext
 const ToggleContext = React.createContext()
+ToggleContext.displayName = 'ToggleContext'
 
 function Toggle({children}) {
   const [on, setOn] = React.useState(false)
@@ -27,7 +28,7 @@ function Toggle({children}) {
 // 💰 `const context = React.useContext(ToggleContext)`
 // 📜 https://reactjs.org/docs/hooks-reference.html#usecontext
 
-const useToggleContext = () => {
+const useToggle = () => {
   const context = React.useContext(ToggleContext)
   if (context === undefined) {
     throw new Error('useToggleContext must be used within a Toggle component')
@@ -36,20 +37,20 @@ const useToggleContext = () => {
 }
 
 function ToggleOn({children}) {
-  const {on} = useToggleContext()
+  const {on} = useToggle()
 
   return on ? children : null
 }
 
 // 🐨 do the same thing to this that you did to the ToggleOn component
 function ToggleOff({children}) {
-  const {on} = useToggleContext()
+  const {on} = useToggle()
   return on ? null : children
 }
 
 // 🐨 get `on` and `toggle` from the ToggleContext with `useContext`
-function ToggleButton({...props}) {
-  const {on, toggle} = useToggleContext()
+function ToggleButton(props) {
+  const {on, toggle} = useToggle()
   return <Switch on={on} onClick={toggle} {...props} />
 }
 
